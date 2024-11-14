da224f36e03c7e28233820de02ff21828231bbb6
goutham <uppinurigouthamreddy@gmail.com>
2024-11-14 22:11:54 +0530

    CORS policy URL , env variable changed

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const app = express()
 //     origin : process.env.FRONTEND_URL
 // }))
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: process.env.FRONTEND_URL , credentials: true }))
 
 app.use(express.json())
 app.use(cookieParser())
@@ -35,6 +35,7 @@ const PORT = process.env.PORT || 8080
 
 app.get("/",(request,response)=>{
     ///server to client
+    console.log("new request came from ",request.ip)
     response.json({
         message : "Server is running " + PORT
     })
